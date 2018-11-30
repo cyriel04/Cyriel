a4dcc5bc2af1fa40ed81a374101c6ff40185aca4
cyriel04 <cbasilyow@gmail.com>
2018-11-30 19:09:33 +0800

    g

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import Navigation from "./Navigation";
 import "antd/dist/antd.css";
 import "./App.css";
 import Page from "./Page";
-<<<<<<< HEAD
 import Footer from "./Footer";
-=======
-
-// import { Fullpage, Slide, HorizontalSlider } from "fullpage-react";
->>>>>>> 63988aadf33e49828402151b2f986158575d377e
 
 class App extends Component {
 	render() {
@@ -16,10 +11,7 @@ class App extends Component {
 			<div className="App">
 				<Navigation />
 				<Page />
-<<<<<<< HEAD
 				<Footer />
-=======
->>>>>>> 63988aadf33e49828402151b2f986158575d377e
 			</div>
 		);
 	}
